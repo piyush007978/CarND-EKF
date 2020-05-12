b9f509e8b7e56a1ff0577c2eb02ac1402ae8fef7
Piyush Tripathi <piyush007978@gmail.com>
2020-05-12 14:23:13 +0530

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -35,6 +35,7 @@ void KalmanFilter::Update(const VectorXd &z) {
    * TODO: update the state by using Kalman Filter equations
    */
   VectorXd y = z - (H_ * x_);
+  
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
